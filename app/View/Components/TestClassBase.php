37a2eca71e781c7a30f9a8fae084b48acb14fa56
a_yamamoto <a.benn17282002@gmail.com>
2022-02-11 23:38:08 +0900

    クラスベースのコンポーネント(初期値・属性追加)

--- app/View/Components/TestClassBase.php
+++ app/View/Components/TestClassBase.php
@@ -6,14 +6,20 @@ use Illuminate\View\Component;
 
 class TestClassBase extends Component
 {
+    // public変数(制限なし)
+    public $classBaseMessage;
+    public $defaultMessage;
+
     /**
      * Create a new component instance.
      *<変数の設定>
      * @return void
      */
-    public function __construct()
+    public function __construct($classBaseMessage,$defaultMessage="初期値です")
     {
-        //
+        // Instanceからオブジェクトの変数にアクセスし、変数化
+        $this->classBaseMessage = $classBaseMessage;
+        $this->defaultMessage = $defaultMessage;
     }
 
     /**
--- resources/views/components/test-class-base.blade.php
+++ resources/views/components/test-class-base.blade.php
@@ -1,3 +1,9 @@
+{{-- view/component/test-class-component,
+app/view/component/testClassBaseからの読み込み --}}
 <div class="border-2 shadow-md w-1/4 p-2 bg-cyan-300">
     クラスベースのコンポーネントテスト
+
+    {{-- Default値と変更値の確認 --}}
+    <div class="border-2 shadow-md  p-2 bg-teal-100">{{ $classBaseMessage }}</div>
+    <div class="border-2 shadow-md  p-2 bg-violet-300">{{ $defaultMessage }}</div>
 </div>
