5508fc24d4b35b77c6cb7e952ad10a22751c9cc8
a_yamamoto <a.benn17282002@gmail.com>
2022-02-12 16:10:35 +0900

    サービスコンテナの依存性解決

--- app/Http/Controllers/LifeCycleTestController.php
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -17,7 +17,76 @@ class LifeCycleTestController extends Controller
         // ライフサイクルの生成->変数に格納
         $test =app()->make('LifeCycleTest');
 
+        /*---------インスタンス実行型--------*/
+
+        $message1 = new Message1();
+        $sample1 = new Sample1($message1);
+        $sample1->run();
+
+        /*----------------------------------*/
+
+        /*---------サービスコンテナ型---------*/
+
+        // Sample2クラスから結合
+        app()->bind('sample',Sample2::class);
+        // ライフサイクル生成
+        $sample2 = app()->make('sample');
+        $sample2->run();
+
+        /*----------------------------------*/
+
         // 生成したライフサイクルとServiceContainerを表示
         dd($test,app());
+    }
+}
+
+/* Messageを取得するクラス(インスタンス実行型) */
+class Sample1
+{
+    public $message1;
+    // 初期化(Message1 $meesages1)
+    public function __construct(Message1 $message1)
+    {
+        // classのmessage1プロパティに$message1を格納
+        $this->message1 =$message1;
+    }
+
+    // オブジェクトより$message1を取得し,messageを送付
+    public function run()
+    {
+        $this->message1->send();
+    }
+}
+
+/* Messageを取得するクラス(サービスコンテナ型) */
+class Sample2
+{
+    public $message2;
+    public function __construct(Message2 $message2)
+    {
+        $this->message2 =$message2;
+    }
+
+    public function run()
+    {
+        $this->message2->send();
+    }
+}
+
+/* Message作成用クラス(インスタンス実行型) */
+class Message1
+{
+    public function send()
+    {
+        echo('インスタンス実行後の表示<br/>');
+    }
+}
+
+/* Message作成用クラス(サービスコンテナ型) */
+class Message2
+{
+    public function send()
+    {
+        echo('サービスコンテナを使ったパターン');
     }
 }
