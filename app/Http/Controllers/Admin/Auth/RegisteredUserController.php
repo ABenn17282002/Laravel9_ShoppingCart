38cbdc8ba8c85837fe47c990a302ee1b55a82dd3
a_yamamoto <a.benn17282002@gmail.com>
2022-02-14 22:42:51 +0900

    Laravelマルチ認証(Admin/OwnerController作成・修正)

---
+++ app/Http/Controllers/Admin/Auth/RegisteredUserController.php
@@ -0,0 +1,60 @@
+<?php
+
+// Admin\Auth
+namespace App\Http\Controllers\Admin\Auth;
+
+use App\Http\Controllers\Controller;
+// Adminモデルの使用
+use App\Models\Admin;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display the registration view.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        // admin用に編集
+        return view('admin.auth.register');
+    }
+
+    /**
+     * Handle an incoming registration request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            // admins_tableからの読み込み
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:admins'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        // Adminモデルの使用
+        $user = Admin::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        // admin用に編集
+        return redirect(RouteServiceProvider::ADMIN_HOME);
+    }
+}
---
+++ app/Http/Controllers/Owner/Auth/EmailVerificationPromptController.php
@@ -0,0 +1,25 @@
+<?php
+
+// \Owner\Auth
+namespace App\Http\Controllers\Owner\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Http\Request;
+
+class EmailVerificationPromptController extends Controller
+{
+    /**
+     * Display the email verification prompt.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return mixed
+     */
+    public function __invoke(Request $request)
+    {
+        return $request->user()->hasVerifiedEmail()
+                    // owner用に編集
+                    ? redirect()->intended(RouteServiceProvider::OWNER_HOME)
+                    : view('owner.auth.verify-email');
+    }
+}
