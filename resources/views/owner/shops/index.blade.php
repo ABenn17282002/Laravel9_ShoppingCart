4ab194772ee723b4f3022ad3a33b9321740aa5a1
a_yamamoto <a.benn17282002@gmail.com>
2022-03-03 00:01:10 +0900

    ShopIndex(ShopIndex画面の作成)

--- resources/views/owner/shops/index.blade.php
+++ resources/views/owner/shops/index.blade.php
@@ -9,9 +9,35 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    {{-- shops_tableから名前を取得 --}}
+                    {{-- foreach構文でshops_tableからデータを取得 --}}
                     @foreach ($shops as $shop)
-                    {{ $shop->name }}
+                        <div class="w-1/2 p-4">
+                            {{-- shop_id取得→編集ページ --}}
+                            <a href="{{ route('owner.shops.edit', ['shop' => $shop->id ])}}">
+                                <div class="border rounded-md p-4">
+                                    {{-- 販売中 or 販売停止中 --}}
+                                    <div class="mb-4">
+                                        @if($shop->is_selling)
+                                            <span class="border p-2 rounded-md bg-blue-400 text-white">販売中</span>
+                                        @else
+                                            <span class="border p-2 rounded-md bg-red-400 text-white">停止中</span>
+                                        @endif
+                                    </div>
+                                    {{-- shop名 --}}
+                                    <div class="text-xl">
+                                        {{ $shop->name }}
+                                    </div>
+                                    {{-- 画像の取得 --}}
+                                    <div>
+                                        @if(empty($shop->filename))
+                                            <img src="{{ asset('images/no_image.jpg')}}">
+                                        @else
+                                            <img src="{{ asset('storage/shops/' . $shop->filename)}}">
+                                        @endif
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
                     @endforeach
                 </div>
             </div>
