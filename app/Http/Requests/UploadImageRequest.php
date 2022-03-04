0a87fb2f52f49fa649fde76363164954fb9fef74
a_yamamoto <a.benn17282002@gmail.com>
2022-03-04 23:35:29 +0900

    フォームリクエスト

---
+++ app/Http/Requests/UploadImageRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UploadImageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // authorize:true
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            // 画像のUpload形式:jpeg,jpg,pngのみ、最大:2MBまで
+            'image'=>'image|mimes:jpg,jpeg,png|max:2048',
+        ];
+    }
+
+    /**
+    * Error Message
+    */
+    public function messages()
+    {
+        return [
+            'image' => '指定されたファイルが画像ではありません。',
+            'mines' => '指定された拡張子（jpg/jpeg/png）ではありません。',
+            'max' => 'ファイルサイズは2MB以内にしてください。',
+        ];
+    }
+}
