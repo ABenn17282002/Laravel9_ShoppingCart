2f369a60c979ab90d238e8e381d9b89eabe2da3c
a_yamamoto <a.benn17282002@gmail.com>
2022-03-13 11:45:53 +0900

    ProductIndex

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -2,11 +2,43 @@
 
 namespace App\Http\Controllers\Owner;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Http\Controllers\Controller;
+// 認証モデルの使用
+use Illuminate\Support\Facades\Auth;
+// Image,Product,Owner,products,SecondaryCategoryモデルの使用
+Use App\Models\Image;
+use App\Models\Product;
+use App\Models\Owner;
+use App\Models\SecondaryCategory;
 class ProductController extends Controller
 {
+    /* コンストラクタの設定 */
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        // コントローラミドルウェア
+        $this->middleware(function ($request, $next) {
+
+            // 製品IDの取得
+            $id = $request->route()->parameter('product');
+            // null判定
+            if(!is_null($id)){
+                // shopに紐づくOwnerIdの取得
+                $productsOwnerId= Product::findOrFail($id)->shop->owner->id;
+                // 文字列→数値に変換
+                $proudcutId = (int)$productsOwnerId;
+                // 製品IDが認証済IDでない場合
+                if($proudcutId  !== Auth::id()){
+                    abort(404); // 404画面表示
+                }
+            }
+
+            return $next($request);
+        });
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +46,12 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        // shopに紐づく製品の認証済IDの取得
+        $products = Owner::findOrFail(Auth::id())->shop->product;
+
+        // owner/products/index.balde.phpにproducts変数付で返す
+        return view('owner.products.index',
+        compact('products'));
     }
 
     /**
