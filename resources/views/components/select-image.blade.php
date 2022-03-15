0d93af7db63ad9b0a5a88eaed469479a12029799
a_yamamoto <a.benn17282002@gmail.com>
2022-03-15 23:47:53 +0900

    micromodal(商品選択)

--- resources/views/components/select-image.blade.php
+++ resources/views/components/select-image.blade.php
@@ -1,24 +1,55 @@
-<div class="modal micromodal-slide" id="modal-1" aria-hidden="true">
+{{-- 取得する画像用の設定 --}}
+@php
+ if($name ==='image1'){$modal ="modal-1" ;}
+ if($name ==='image2'){$modal ="modal-2" ;}
+ if($name ==='image3'){$modal ="modal-3" ;}
+ if($name ==='image4'){$modal ="modal-4" ;}
+@endphp
+
+{{-- modal-1 => {{ $modal }} --}}
+<div class="modal micromodal-slide" id="{{ $modal }}" aria-hidden="true">
     <div class="modal__overlay" tabindex="-1" data-micromodal-close>
-      <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="modal-1-title">
+      <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="{{ $modal }}-title">
         <header class="modal__header">
-          <h2 class="modal__title" id="modal-1-title">
-            Micromodal
+          <h2 class="modal__title" id="{{ $modal }}-title">
+            ファイルを選択してください。
           </h2>
           {{-- type="button"がないと送信になる。 --}}
           <button type="button" class="modal__close" aria-label="Close modal" data-micromodal-close></button>
         </header>
-        <main class="modal__content" id="modal-1-content">
-          <p>
-            Try hitting the <code>tab</code> key and notice how the focus stays within the modal itself. Also, <code>esc</code> to close modal.
-          </p>
+        <main class="modal__content" id="{{ $modal }}-content">
+            {{-- 画像をflexで横並びにする --}}
+            <div class="flex flex-wrap">
+                {{-- foreach構文でimages_tableからデータを取得 --}}
+                 @foreach ($images as $image)
+                        {{-- width:1/2→1/4に変更, padding-2, md:p-4に変更 --}}
+                        <div class="w-1/4 p-2 md:p-4">
+                            {{-- data-idを利用し、JSでe.target.dataset.**で
+                            データを取得するようにする。--}}
+                            <img class="image" data-id="{{ $name }}_{{ $image->id }}"
+                            data-file="{{ $image->filename }}"
+                            data-path="{{ asset('storage/products/') }}"
+                            data-modal="{{ $modal }}"
+                            src="{{ asset('storage/products/' . $image->filename)}}">
+                            {{-- 画像title --}}
+                            <div class="text-gray-700">{{ $image->title }}</div>
+                        </div>
+                @endforeach
+            </div>
         </main>
         <footer class="modal__footer">
-          <button type="button" class="modal__btn modal__btn-primary">Continue</button>
-          <button type="button" class="modal__btn" data-micromodal-close aria-label="Close this dialog window">Close</button>
+          <button type="button" class="modal__btn" data-micromodal-close aria-label="閉じる">閉じる</button>
         </footer>
       </div>
     </div>
   </div>
-{{-- buttonをつける --}}
-  <a data-micromodal-trigger="modal-1" href='javascript:void(0);'>Open Modal Dialog</a>
+
+  {{-- プレビューエリアとinputタグ(hidden) --}}
+  <div class="flex justify-around items-center mb-4">
+    {{-- 開くボタン --}}
+    <a data-micromodal-trigger="{{ $modal }}" href='javascript:void(0);'>ファイルを選択</a>
+    <div class="w-1/4">
+    <img id="{{ $name }}_thumbnail" src="">
+    </div>
+    </div>
+    <input id="{{ $name }}_hidden" type="hidden" name="{{ $name }}" value="">
--- resources/views/owner/products/create.blade.php
+++ resources/views/owner/products/create.blade.php
@@ -33,7 +33,11 @@
                                         </select>
                                     </div>
                                 </div>
-                                <x-select-image  name="image1" />
+                                {{-- コンポーネントより選択した画像を取得 --}}
+                                <x-select-image :images="$images" name="image1" />
+                                <x-select-image :images="$images" name="image2" />
+                                <x-select-image :images="$images" name="image3" />
+                                <x-select-image :images="$images" name="image4" />
                                 <div class="p-2 w-full flex justify-around mt-4">
                                         {{-- 戻る --}}
                                         <button type="button" onclick="location.href='{{ route('owner.products.index')}}'" class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
@@ -47,4 +51,32 @@
             </div>
         </div>
     </div>
+    <script>
+        'use strict'
+        // 全てのimagetagを取得
+        const images=document.querySelectorAll('.image');
+
+        // 取得したtagを1つずつ取得
+        images.forEach(image => {
+            // clickしたら発火
+            image.addEventListener('click',function(e){
+            /* Image_IDの取得　*/
+            //data-idの6文字の取得
+            const imageName = e.target.dataset.id.substr(0, 6);
+            // 6文字カット
+            const imageId = e.target.dataset.id.replace(imageName + '_', '');
+            // fileの取得
+            const imageFile = e.target.dataset.file;
+            // pathの取得
+            const imagePath = e.target.dataset.path;
+            const modal = e.target.dataset.modal;
+
+            // サムネイルとinput type=hiddenのvalueに設定
+            document.getElementById(imageName + '_thumbnail').src = imagePath + '/' + imageFile;
+            document.getElementById(imageName + '_hidden').value = imageId;
+
+    }, )
+    })
+
+    </script>
 </x-app-layout>
