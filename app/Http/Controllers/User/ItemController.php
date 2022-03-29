dfc829db09bec13f83e6d679a064a87450ee75ff
a_yamamoto <a.benn17282002@gmail.com>
2022-03-29 21:58:47 +0900

    ItemList_Query(商品一覧の完成)

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -14,7 +14,6 @@ class ItemController extends Controller
     // indexページの表示
     public function index()
     {
-        // $products = Product::all();
 
         /* Stockの合計をグループ化->数量が1以上 */
         $stocks = DB::table('t_stocks')
@@ -32,13 +31,26 @@ class ItemController extends Controller
         })
         // Join shops on products.shop_id = shops.id
         ->join('shops', 'products.shop_id', '=', 'shops.id')
+        // Eloquenからクエリービルダーに変更したため、tableの紐づくidを同士を結ぶ
+        ->join('secondary_categories', 'products.secondary_category_id', '=',
+        'secondary_categories.id')
+        ->join('images as image1', 'products.image1', '=', 'image1.id')
+        ->join('images as image2', 'products.image2', '=', 'image2.id')
+        ->join('images as image3', 'products.image3', '=', 'image3.id')
+        ->join('images as image4', 'products.image4', '=', 'image4.id')
         // where shops.is_selling =1
         ->where('shops.is_selling', true)
         // where products.is_selling =1
         ->where('products.is_selling', true)
+        // select products.id,name,price,sort_order,information,
+        // secondary_categories.name,image1.filename
+        ->select('products.id as id', 'products.name as name', 'products.price'
+        ,'products.sort_order as sort_order'
+        ,'products.information', 'secondary_categories.name as category'
+        ,'image1.filename as filename')
         ->get();
 
-        dd($stocks,$products);
+        // dd($stocks,$products);
 
 
         return view('user.index',\compact('products'));
