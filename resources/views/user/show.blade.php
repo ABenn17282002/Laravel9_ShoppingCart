721c3e8c8f040b6e027c1515c2da923596e5fa07
a_yamamoto <a.benn17282002@gmail.com>
2022-04-02 22:04:57 +0900

    Shop_modalWindow

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -109,6 +109,28 @@
             </div>
         </div>
     </div>
+    {{-- modal-window --}}
+    <div class="modal micromodal-slide " id="modal-1" aria-hidden="true">
+        {{-- 重ね順の調整 --}}
+            <div class="z-10 modal__overlay" tabindex="-1" data-micromodal-close>
+                <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="modal-1-title">
+                    <header class="modal__header">
+                        <h2 class="text-xl text-gray-700" id="modal-1-title">
+                            {{ $product->shop->name }}
+                        </h2>
+                        <button class="modal__close" aria-label="Close modal" data-micromodal-close></button>
+                    </header>
+                    <main class="modal__content" id="modal-1-content">
+                        <p>
+                            {{ $product->shop->information }}
+                        </p>
+                    </main>
+                    <footer class="modal__footer">
+                        <button class="modal__btn" data-micromodal-close aria-label="Close this dialog window">閉じる</button>
+                    </footer>
+                </div>
+            </div>
+    </div>
     <script src="{{ mix('js/swiper.js')}}"></script>
 </x-app-layout>
 
