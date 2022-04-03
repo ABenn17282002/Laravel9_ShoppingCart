9d75b71c7fec1a78682470614b95bb430c2d4b5f
a_yamamoto <a.benn17282002@gmail.com>
2022-04-03 16:34:35 +0900

    cartadd_debug1

---
+++ app/Http/Controllers/User/CartController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    // 商品追加のためのメソッド
+    public function add(Request $request)
+    {
+        dd($request);
+    }
+}
--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -72,20 +72,26 @@
                                 <div>
                                     <span class="title-font font-medium text-2xl text-gray-900">{{ number_format($product->price) }}</span><span class="text-sm text-gray-700">円(税込)</span>
                                 </div>
-                                {{-- 数量 --}}
-                                <div class="flex ml-auto items-center">
-                                    <span class="mr-3">数量</span>
-                                    <div class="relative">
-                                        <select  name="quantity" class="rounded mr-3 border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
-                                            {{-- 数量の取得 --}}
-                                            @for ($i = 1; $i <= $quantity; $i++)
-                                            <option value="{{ $i }}">{{ $i }} </option>
-                                            @endfor
-                                        </select>
+                                {{-- Cart追加のためのフォーム追加 --}}
+                                <form method="post" action="{{ route('user.cart.add')}}">
+                                    @csrf
+                                    {{-- 数量 --}}
+                                    <div class="flex ml-auto items-center">
+                                        <span class="mr-3">数量</span>
+                                        <div class="relative">
+                                            <select  name="quantity" class="rounded mr-3 border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
+                                                {{-- 数量の取得 --}}
+                                                @for ($i = 1; $i <= $quantity; $i++)
+                                                <option value="{{ $i }}">{{ $i }} </option>
+                                                @endfor
+                                            </select>
+                                        </div>
                                     </div>
-                                </div>
-                                {{-- カートに入れる --}}
-                                <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">カートに入れる</button>
+                                    {{-- カートに入れる --}}
+                                    <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">カートに入れる</button>
+                                    {{-- フォームに渡すためのidを取得 --}}
+                                    <input type="hidden" name="product_id" value="{{ $product->id}}">
+                                </form>
                             </div>
                         </div>
                     </div>
