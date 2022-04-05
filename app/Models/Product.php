4077ca232e51915309cf7f48e67bf52eecf62dec
a_yamamoto <a.benn17282002@gmail.com>
2022-04-05 23:43:59 +0900

    Local Scope

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,8 @@ use App\Models\Stock;
 //  Userモデルの使用
 use App\Models\User;
 use App\Models\SecondaryCategory;
+// DBクラスのインポート
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -93,5 +95,48 @@ class Product extends Model
     {
         return $this->belongsToMany(User::class,'carts')
         ->withPivot(['id', 'quantity']);
+    }
+
+    /**
+     * 商品在庫が1以上のものを表示するクエリスコープを設定
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeAvailableItems($query)
+    {
+        /* 商品在庫数が1以上のもの */
+        $stocks = DB::table('t_stocks')->select('product_id',
+        // select内でsumを使うためクエリビルダのDB::rawで対応
+        DB::raw('sum(quantity) as quantity'))
+        ->groupBy('product_id')
+        ->having('quantity', '>', 1);
+
+        /* 上記queryに合致する商品詳細を表示 */
+        return $query
+        // $stockのクエリーをSubqueryとして使用
+        ->joinSub($stocks, 'stock', function($join){
+            // Join product on products.id = stock.product_id
+            $join->on('products.id', '=', 'stock.product_id');
+            })
+            // Join shops on products.shop_id = shops.id
+            ->join('shops', 'products.shop_id', '=', 'shops.id')
+            // Eloquenからクエリービルダーに変更したため、tableの紐づくidを同士を結ぶ
+            ->join('secondary_categories', 'products.secondary_category_id', '=',
+            'secondary_categories.id')
+            ->join('images as image1', 'products.image1', '=', 'image1.id')
+            ->join('images as image2', 'products.image2', '=', 'image2.id')
+            ->join('images as image3', 'products.image3', '=', 'image3.id')
+            ->join('images as image4', 'products.image4', '=', 'image4.id')
+            // where shops.is_selling =1
+            ->where('shops.is_selling', true)
+            // where products.is_selling =1
+            ->where('products.is_selling', true)
+            // select products.id,name,price,sort_order,information,
+            // secondary_categories.name,image1.filename
+            ->select('products.id as id', 'products.name as name', 'products.price'
+            ,'products.sort_order as sort_order'
+            ,'products.information', 'secondary_categories.name as category'
+            ,'image1.filename as filename');
     }
 }
