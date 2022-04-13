58e597b44a90f43dda6de0b076e6dcfedec0d05a
a_yamamoto <a.benn17282002@gmail.com>
2022-04-13 22:41:41 +0900

    カート情報の配列

--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Services;
-
+// Productモデルの使用
 use App\Models\Product;
+// Cartモデルの使用
+use App\Models\Cart;
 
 class CartService
 {
@@ -11,12 +13,40 @@ class CartService
         //空の配列を準備
         $products = [];
 
-        dd($items);
-
         // カート内の商品を一つずつ処理
         foreach($items as $item){
+
+            /* Owner情報の取得 */
+            // 製品ID取得
+            $p = Product::findOrFail($item->product_id);
+            // オーナー情報を取得し、配列化
+            $owner = $p->shop->owner->select('name', 'email')->first()->toArray();
+            //連想配列の値を取得
+            $values = array_values($owner);
+            // 連想配列のkeyを設定
+            $keys = ['ownerName', 'email'];
+            // オーナー情報を取得
+            $ownerInfo = array_combine($keys, $values);
+            // dd($ownerInfo);
+
+            /* 商品・在庫情報の取得 */
+            // 商品情報の配列化
+            $product = Product::where('id', $item->product_id)
+            ->select('id', 'name', 'price')->get()->toArray();
+            // 在庫数の配列化
+            $quantity = Cart::where('product_id', $item->product_id)
+            ->select('quantity')->get()->toArray();
+            // dd($ownerInfo, $product, $quantity);
+
+            // 配列の結合
+            $result = array_merge($product[0], $ownerInfo, $quantity[0]);
+            // dd($result);
+
+            // 取得したowner・商品情報等を配列に追加
+            array_push($products, $result);
         }
 
+        dd($products);
         return $products;
     }
 }
